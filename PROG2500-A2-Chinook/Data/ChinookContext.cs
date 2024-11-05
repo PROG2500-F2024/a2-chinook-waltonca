e94a448169deed6161db5958bf251319983e6d99
walton <zjxcdut@gmail.com>
2024-11-05 15:55:45 -0400

    Save connection string into App.config

--- PROG2500-A2-Chinook/Data/ChinookContext.cs
+++ PROG2500-A2-Chinook/Data/ChinookContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using PROG2500_A2_Chinook.Models;
 
@@ -38,7 +39,7 @@ public partial class ChinookContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Chinook;Integrated Security=True;Trust Server Certificate=False;");
+        => optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["ChinookDBConn"].ConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
