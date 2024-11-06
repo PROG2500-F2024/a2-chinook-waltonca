9302cf7de288446d11624835adc8d6d35fd454a0
walton <zjxcdut@gmail.com>
2024-11-05 22:35:38 -0400

    Add scaffold-dbcontext command in dbcontext class

--- PROG2500-A2-Chinook/Data/ChinookContext.cs
+++ PROG2500-A2-Chinook/Data/ChinookContext.cs
@@ -6,6 +6,10 @@ using PROG2500_A2_Chinook.Models;
 
 namespace PROG2500_A2_Chinook.Data;
 
+/// <summary>
+/// scaffold-dbcontext command :
+/// scaffold-dbcontext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Chinook;Integrated Security=True;Trust Server Certificate=False;" Microsoft.EntityFrameworkCore.SqlServer -contextdir Data -outputdir Models/Generated -contextnamespace PROG2500_A2_Chinook.Data -namespace PROG2500_A2_Chinook.Models -force
+/// </summary>
 public partial class ChinookContext : DbContext
 {
     public ChinookContext()
